6c61ddaccab8e53b0d97ae9c7a828809e06dfc28
Patrick Ellul <pellul@assemblypayments.com>
2018-04-21 18:49:03 +1000

    a little release helper file

---
+++ release.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/sh
+
+VERSION=2.1.1
+
+cd releases
+
+cp -r ../KebabPos/bin/Debug KebabPos-$VERSION
+zip -r KebabPos-2.1.1.zip KebabPos-$VERSION
+
+cp -r ../MotelPos/bin/Debug MotelPos-$VERSION
+zip -r MotelPos-2.1.1.zip MotelPos-$VERSION
+
+cp -r ../TablePos/bin/Debug TablePos-$VERSION
+zip -r TablePos-2.1.1.zip TablePos-$VERSION
+
+cd -
