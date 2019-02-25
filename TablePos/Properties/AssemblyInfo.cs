f84e34b94a72867da5b92320231f464c9c8c733b
Metin Avci <mavci@assemblypayments.com>
2019-02-25 14:24:51 +1100

    Update SPI Library to v2.4.5

--- TablePos/Properties/AssemblyInfo.cs
+++ TablePos/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("2.1.1.*")]
+[assembly: AssemblyVersion("2.4.5.*")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
@@ -26,5 +26,5 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyKeyFile("")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "SPIClient.dll.config", Watch = true)]
-[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.4.5")]
 
